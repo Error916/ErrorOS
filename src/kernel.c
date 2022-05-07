2ce6db7c8b4c7d1ca8ab17693970be7a0fa03d97
Error916 <ettorenigma@gmail.com>
2022-05-07 23:00:56 +0200

    give kernel access to memory map and free bootservices from the bootloader

--- src/kernel.c
+++ src/kernel.c
@@ -1,24 +1,22 @@
+#include <stdint.h>
 #include "BasicRenderer.h"
 #include "cstr.h"
 
-void _start(FrameBuffer* framebuffer, PSF1_FONT* psf1_font){
+typedef struct {
+	FrameBuffer* framebuffer;
+	PSF1_FONT* psf1_font;
+	void* mMap;
+	uint64_t mMapSize;
+	uint64_t mMapDescSize;
+} BootInfo;
+
+void _start(BootInfo* bootinfo){
 
 	Point CursorPosition = {0, 0};
 	BasicRenderer newRenderer;
-	BasicRendererConstructor(&newRenderer, &CursorPosition, framebuffer, psf1_font);
+	BasicRendererConstructor(&newRenderer, &CursorPosition, bootinfo->framebuffer, bootinfo->psf1_font);
 
 	Print(&newRenderer, "Hello From Kernel");
-	newRenderer.CursorPosition->X = 0;
-	newRenderer.CursorPosition->Y = 16;
-	Print(&newRenderer, ito_string(-1234));
-	newRenderer.CursorPosition->X = 0;
-	newRenderer.CursorPosition->Y = 32;
-	Print(&newRenderer, uto_string(1234));
-	newRenderer.CursorPosition->X = 0;
-	newRenderer.CursorPosition->Y = 48;
-	Print(&newRenderer, dto_string(-3.14));
-	newRenderer.CursorPosition->X = 0;
-	newRenderer.CursorPosition->Y = 64;
 
 	return ;
 }
