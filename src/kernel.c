99f036d07b1d90ca44d5f671f79c90745a6feabc
Error916 <ettorenigma@gmail.com>
2022-05-07 15:27:07 +0200

    Make so the bootloader would pass the FrameBuffer and the PSF1 Font to the kernel and implemented a rudimental Print function

--- src/kernel.c
+++ src/kernel.c
@@ -1,3 +1,62 @@
-int _start(){
-	return 123;
+typedef unsigned long long size_t;
+
+typedef struct {
+	void* BaseAddress;
+	size_t BufferSize;
+	unsigned int Width;
+	unsigned int Height;
+	unsigned int PixelsPerScanLine;
+} FrameBuffer;
+
+typedef struct {
+	unsigned char magic[2];
+	unsigned char mode;
+	unsigned char charsize;
+} PSF1_HEADER;
+
+typedef struct {
+	PSF1_HEADER* psf1_Header;
+	void* glyphBuffer;
+} PSF1_FONT;
+
+typedef struct {
+	unsigned int X;
+	unsigned int Y;
+} Point;
+
+void putChar(FrameBuffer* framebuffer, PSF1_FONT* psf1_font, unsigned int color, char chr, unsigned int xOff, unsigned int yOff){
+	unsigned int* pixPtr = (unsigned int*) framebuffer->BaseAddress;
+	char* fontPtr = psf1_font->glyphBuffer + (chr * psf1_font->psf1_Header->charsize);
+	for(unsigned long y = yOff; y < yOff + 16; ++y){
+		for(unsigned long x = xOff; x < xOff + 8; ++x){
+			if((*fontPtr & (0b10000000 >> (x - xOff))) > 0){
+				*(unsigned int*)(pixPtr + x + (y * framebuffer->PixelsPerScanLine)) = color;
+			}
+		}
+		fontPtr++;
+	}
+}
+
+Point CursorPosition;
+void Print(FrameBuffer* framebuffer, PSF1_FONT* psf1_font, unsigned int color, char* str){
+	unsigned int x = 0;
+	char* chr = str;
+	while(*chr != 0){
+		putChar(framebuffer, psf1_font, color, *chr, CursorPosition.X, CursorPosition.Y);
+		CursorPosition.X += 8;
+		if(CursorPosition.X + 8 > framebuffer->Width){
+			CursorPosition.X = 0;
+			CursorPosition.Y += 16;
+		}
+		chr++;
+	}
+}
+
+void _start(FrameBuffer* framebuffer, PSF1_FONT* psf1_font){
+	CursorPosition.X = 0;
+	CursorPosition.Y = 0;
+
+	Print(framebuffer, psf1_font, 0xffffffff, "Hello From Kernel");
+
+	return ;
 }
