4c857ae7abb639c268224183118c3ff12ea88755
Error916 <ettorenigma@gmail.com>
2022-05-07 23:21:43 +0200

    use the mMap info to print the efi use of memory

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,12 @@
 #include <stdint.h>
 #include "BasicRenderer.h"
 #include "cstr.h"
+#include "efiMemory.h"
 
 typedef struct {
 	FrameBuffer* framebuffer;
 	PSF1_FONT* psf1_font;
-	void* mMap;
+	EFI_MEMORY_DESCRIPTOR* mMap;
 	uint64_t mMapSize;
 	uint64_t mMapDescSize;
 } BootInfo;
@@ -16,7 +17,18 @@ void _start(BootInfo* bootinfo){
 	BasicRenderer newRenderer;
 	BasicRendererConstructor(&newRenderer, &CursorPosition, bootinfo->framebuffer, bootinfo->psf1_font);
 
-	Print(&newRenderer, "Hello From Kernel");
+	uint64_t mMapEntries = bootinfo->mMapSize / bootinfo->mMapDescSize;
+	for(int i = 0; i < mMapEntries; ++i){
+		EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)((uint64_t)bootinfo->mMap + (i * bootinfo->mMapDescSize));
+		newRenderer.CursorPosition->X = 0;
+		newRenderer.CursorPosition->Y += 16;
+		Print(&newRenderer, EFI_MEMORY_TYPE_STRINGS[desc->type]);
+		newRenderer.Color = 0xffff00ff;
+		Print(&newRenderer, " ");
+		Print(&newRenderer, uto_string(desc->numPages * 4096 / 1024));
+		Print(&newRenderer, " KB");
+		newRenderer.Color = 0xffffffff;
+	}
 
 	return ;
 }
