469413126e7b31bed175ad4601d938c3a9d75f12
Error916 <ettorenigma@gmail.com>
2022-05-23 10:42:30 +0200

    create read and write true ahci

--- kernel/src/ahci/ahci.c
+++ kernel/src/ahci/ahci.c
@@ -62,6 +62,15 @@ void AHCIDriverConstructor(AHCIDriver* ahciDriver, PCIDeviceHeader* pciBaseAddre
 	for(int i = 0; i < ahciDriver->portCount; ++i){
 		Port* port = ahciDriver->ports[i];
 		AHCIPortConfigure(port);
+
+		port->buffer = (uint8_t*)RequestPage(&GlobalAllocator);
+		memset(port->buffer, 0, 0x1000);
+
+		AHCIPortRead(port, 0, 4, port->buffer);
+		for(int i = 0; i < 1024; ++i){
+			PutCharS(GlobalRenderer, port->buffer[i]);
+		}
+		Next(GlobalRenderer);
 	}
 
 }
@@ -106,4 +115,114 @@ void AHCIProbePorts(AHCIDriver* ahciDriver){
 			}
 		}
 	}
+}
+
+bool AHCIPortRead(Port* port, uint64_t sector, uint32_t sectorCount, void* buffer){
+	/* Spin Lock to make the function thread safe*/
+	uint64_t spin = 0;
+	while((port->hbaPort->taskFileData & (ATA_DEV_BUSY | ATA_DEV_DRQ)) && spin < 1000000) ++spin;
+	if(spin == 1000000) return false; // port hung
+
+	uint32_t sectorL = (uint32_t)sector;
+	uint32_t sectorH = (uint32_t)(sector >> 32);
+
+	port->hbaPort->interruptStatus  = (uint32_t) -1; // clear pending interrupt bits
+
+	HBACommandHeader* cmdHeader = (HBACommandHeader*)port->hbaPort->commandListBase;
+	cmdHeader->commandFISLength = sizeof(FIS_REG_H2D) / sizeof(uint32_t); // command FIS size
+	cmdHeader->write = 0; // this is a read
+	cmdHeader->prdtLength = 1;
+
+	HBACommandTable* commandTable = (HBACommandTable*)(cmdHeader->commandTableBaseAddress);
+	memset(commandTable, 0, sizeof(HBACommandTable) + (cmdHeader->prdtLength - 1) * sizeof(HBAPRDTEntry));
+
+	commandTable->prdtEntry[0].dataBaseAddress = (uint32_t)(uint64_t)buffer;
+	commandTable->prdtEntry[0].dataBaseAddressUpper = (uint32_t)((uint64_t)buffer >> 32);
+	commandTable->prdtEntry[0].byteCount = (sectorCount << 9) - 1; // 512 bytes per sector
+	commandTable->prdtEntry[0].interruptOnCompletion = 1; // TODO: implement this interrupt
+
+	FIS_REG_H2D* cmdFIS = (FIS_REG_H2D*)(&commandTable->commandFIS);
+
+	cmdFIS->fisType = FIS_TYPE_REG_H2D;
+	cmdFIS->commandControl = 1; // command
+	cmdFIS->command = ATA_CMD_READ_DMA_EX;
+
+	cmdFIS->lba0 = (uint8_t)sectorL;
+	cmdFIS->lba1 = (uint8_t)(sectorL >> 8);
+	cmdFIS->lba2 = (uint8_t)(sectorL >> 16);
+	cmdFIS->lba3 = (uint8_t)sectorH;
+	cmdFIS->lba4 = (uint8_t)(sectorH >> 8);
+	cmdFIS->lba5 = (uint8_t)(sectorH >> 16);
+
+	cmdFIS->deviceRegister = 1 << 6; // LBA mode
+
+	cmdFIS->countLow = sectorCount & 0xff;
+	cmdFIS->countHigh = (sectorCount >> 8) & 0xff;
+
+	port->hbaPort->commandIssue = 1;
+
+	/* blocking function untill we make the os asyncronus */
+	while(true){
+		if((port->hbaPort->commandIssue == 0)) break;
+		if(port->hbaPort->interruptStatus & HBA_PxIS_TFES) return false; // read insucesfull
+	}
+
+	if(port->hbaPort->interruptStatus & HBA_PxIS_TFES) return false; // be sure that we dont jump last check
+
+	return true;
+}
+
+bool AHCIPortWrite(Port* port, uint64_t sector, uint32_t sectorCount, void* buffer){
+	/* Spin Lock to make the function thread safe*/
+	uint64_t spin = 0;
+	while((port->hbaPort->taskFileData & (ATA_DEV_BUSY | ATA_DEV_DRQ)) && spin < 1000000) ++spin;
+	if(spin == 1000000) return false; // port hung
+
+	uint32_t sectorL = (uint32_t)sector;
+	uint32_t sectorH = (uint32_t)(sector >> 32);
+
+	port->hbaPort->interruptStatus  = (uint32_t) -1; // clear pending interrupt bits
+
+	HBACommandHeader* cmdHeader = (HBACommandHeader*)port->hbaPort->commandListBase;
+	cmdHeader->commandFISLength = sizeof(FIS_REG_H2D) / sizeof(uint32_t); // command FIS size
+	cmdHeader->write = 1; // this is a write
+	cmdHeader->prdtLength = 1;
+
+	HBACommandTable* commandTable = (HBACommandTable*)(cmdHeader->commandTableBaseAddress);
+	memset(commandTable, 0, sizeof(HBACommandTable) + (cmdHeader->prdtLength - 1) * sizeof(HBAPRDTEntry));
+
+	commandTable->prdtEntry[0].dataBaseAddress = (uint32_t)(uint64_t)buffer;
+	commandTable->prdtEntry[0].dataBaseAddressUpper = (uint32_t)((uint64_t)buffer >> 32);
+	commandTable->prdtEntry[0].byteCount = (sectorCount << 9) - 1; // 512 bytes per sector
+	commandTable->prdtEntry[0].interruptOnCompletion = 1; // TODO: implement this interrupt
+
+	FIS_REG_H2D* cmdFIS = (FIS_REG_H2D*)(&commandTable->commandFIS);
+
+	cmdFIS->fisType = FIS_TYPE_REG_H2D;
+	cmdFIS->commandControl = 1; // command
+	cmdFIS->command = ATA_CMD_WRITE_DMA_EX;
+
+	cmdFIS->lba0 = (uint8_t)sectorL;
+	cmdFIS->lba1 = (uint8_t)(sectorL >> 8);
+	cmdFIS->lba2 = (uint8_t)(sectorL >> 16);
+	cmdFIS->lba3 = (uint8_t)sectorH;
+	cmdFIS->lba4 = (uint8_t)(sectorH >> 8);
+	cmdFIS->lba5 = (uint8_t)(sectorH >> 16);
+
+	cmdFIS->deviceRegister = 1 << 6; // LBA mode
+
+	cmdFIS->countLow = sectorCount & 0xff;
+	cmdFIS->countHigh = (sectorCount >> 8) & 0xff;
+
+	port->hbaPort->commandIssue = 1;
+
+	/* blocking function untill we make the os asyncronus */
+	while(true){
+		if((port->hbaPort->commandIssue == 0)) break;
+		if(port->hbaPort->interruptStatus & HBA_PxIS_TFES) return false; // read insucesfull
+	}
+
+	if(port->hbaPort->interruptStatus & HBA_PxIS_TFES) return false; // be sure that we dont jump last check
+
+	return true;
 }
--- kernel/src/ahci/ahci.h
+++ kernel/src/ahci/ahci.h
@@ -18,6 +18,12 @@
 #define HBA_PxCMD_FRE 0x0010
 #define HBA_PxCMD_ST 0x0001
 #define HBA_PxCMD_FR 0x4000
+#define HBA_PxIS_TFES (1 << 30)
+
+#define ATA_CMD_READ_DMA_EX 0x25
+#define ATA_CMD_WRITE_DMA_EX 0x35
+#define ATA_DEV_BUSY 0x80
+#define ATA_DEV_DRQ 0x08
 
 typedef enum {
 	None = 0,
@@ -26,6 +32,17 @@ typedef enum {
 	PM = 3,
 	SATAPI = 4,
 } PortType;
+
+typedef enum {
+	FIS_TYPE_REG_H2D = 0x27,
+	FIS_TYPE_REG_D2H = 0x34,
+	FIS_TYPE_DMA_ACT = 0x39,
+	FIS_TYPE_DMA_SETUP = 0x41,
+	FIS_TYPE_DATA = 0x46,
+	FIS_TYPE_BIST = 0x58,
+	FIS_TYPE_PIO_SETUP = 0x5F,
+	FIS_TYPE_DEV_BITS = 0xA1,
+} FIS_TYPE;
 
 typedef struct {
 	uint32_t commandListBase;
@@ -67,7 +84,7 @@ typedef struct {
 } HBAMemory;
 
 typedef struct {
-	uint8_t commandFISLenght:5;
+	uint8_t commandFISLength:5;
 	uint8_t atapi:1;
 	uint8_t write:1;
 	uint8_t prefretchable:1;
@@ -84,6 +101,54 @@ typedef struct {
 	uint32_t commandTableBaseAddressUpper;
 	uint32_t rsv1[4];
 } HBACommandHeader;
+
+typedef struct {
+	uint32_t dataBaseAddress;
+	uint32_t dataBaseAddressUpper;
+	uint32_t rsv0;
+
+	uint32_t byteCount:22;
+	uint32_t rsv1:9;
+	uint32_t interruptOnCompletion:1;
+} HBAPRDTEntry;
+
+typedef struct {
+	uint8_t commandFIS[64];
+
+	uint8_t atapiCommand[16];
+
+	uint8_t rsv[48];
+
+	HBAPRDTEntry prdtEntry[];
+} HBACommandTable;
+
+typedef struct {
+	uint8_t fisType;
+
+	uint8_t portMultiplaier:4;
+	uint8_t rsv0:3;
+	uint8_t commandControl:1;
+
+	uint8_t command;
+	uint8_t featureLow;
+
+	uint8_t lba0;
+	uint8_t lba1;
+	uint8_t lba2;
+	uint8_t deviceRegister;
+
+	uint8_t lba3;
+	uint8_t lba4;
+	uint8_t lba5;
+	uint8_t featureHigh;
+
+	uint8_t countLow;
+	uint8_t countHigh;
+	uint8_t isoCommandCompletion;
+	uint8_t control;
+
+	uint8_t rsv1[4];
+} FIS_REG_H2D;
 
 typedef struct {
 	HBAPort* hbaPort;
@@ -105,4 +170,5 @@ void AHCIProbePorts(AHCIDriver* ahciDriver);
 void AHCIPortConfigure(Port* port);
 void AHCIPortStopCMD(Port* port);
 void AHCIPortStartCMD(Port* port);
-
+bool AHCIPortRead(Port* port, uint64_t sector, uint32_t sectorCount, void* buffer);
+bool AHCIPortWrite(Port* port, uint64_t sector, uint32_t sectorCount, void* buffer);
